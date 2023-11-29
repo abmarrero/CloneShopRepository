a3791f6ae9281f42c3f5cfd3d18ed2341a0019b2
abmarrero <abelmarrerol@gmail.com>
2023-11-29 13:31:13 -0500

    show active page

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,12 +1,16 @@
 import { AppBar, Box, Toolbar,Button ,Typography ,Badge , IconButton } from '@mui/material'
 
-import NextLink from 'next/link';
+
 import Link from '@mui/material/Link';
 import { SearchOutlined } from '@mui/icons-material';
 import {ShoppingCartOutlined} from '@mui/icons-material';
+import { useRouter } from 'next/router';
  
 export const Navbar = () => {
-    
+    const {asPath } = useRouter();
+    console.log(asPath) 
+
+
   return (
     <AppBar >
         <Toolbar >
@@ -31,15 +35,15 @@ export const Navbar = () => {
 
             <Box sx={{display: {xs:'none',sm:'block'}}}>
               <Link href='/category/men'>
-                <Button>Hombres</Button>
+                <Button color={ asPath==='/category/men' ? 'primary': 'info'}>Hombres </Button>
               </Link>
             
               <Link href='/category/women'>
-                <Button>mujeres</Button>
+                <Button color={ asPath === '/category/women' ? 'primary': 'info'}>mujeres</Button>
               </Link>
             
               <Link href='/category/kid'>
-                <Button>Niños</Button>
+                <Button color={asPath==='/category/kid' ? 'primary': 'info'}>Niños</Button>
               </Link>
             </Box>
            
