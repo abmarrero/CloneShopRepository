2331cd6cc958751a5a96ccb148e72fbd7c6b69b7
abmarrero <a.ernestomarrerol@gmail.com>
2023-12-08 22:46:12 -0500

    user model

---
+++ models/User.ts
@@ -0,0 +1,26 @@
+import { IUser } from '@/Interfaces/user';
+import mongoose, {Schema,Model,model} from 'mongoose';
+
+const userSchema = new Schema({ 
+
+name:{type: 'string', required: true},
+email:{type: 'string', required: true,unique: true},
+password:{type: 'string', required: true},
+role:[{type: String,
+        enum:{
+            values:['admin','client'],
+            message:'{VALUE} no es un tamaño válido',
+            default: 'client',
+            required:true
+        }
+}],
+
+},{
+    timestamps:true
+});
+
+   
+
+const User:Model<IUser>= mongoose.models.User || model('User' , userSchema);
+
+export default User;
