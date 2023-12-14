809a31276e5ff15dae00480d46d33f0d9c7464a6
abmarrero <a.ernestomarrerol@gmail.com>
2023-12-14 11:57:46 -0500

    front validations

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,6 +1,7 @@
 
 
 import { AuthLayout } from '@/components/layouts'
+import { isEmail } from '@/utils/validations';
 import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { useForm } from 'react-hook-form';
@@ -18,17 +19,29 @@ type Inputs = {
         };
   return (
     <AuthLayout title={'Ingresar'} >
-        <form onSubmit={handleSubmit(onLoginUser)}>
+        <form onSubmit={handleSubmit(onLoginUser)} noValidate>
         <Box sx={{width:350, padding: '10px 20px'}}>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <Typography variant='h1' component='h1'>Iniciar Sesión</Typography>
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField type="email" {...register('email')} label='Correo' variant='filled' fullWidth></TextField>
+                    <TextField type="email" {...register('email',{
+                    required: 'Este campo es requerido',
+                    validate: (val) => isEmail(val)
+                    })}
+                    error={!!errors.email}
+                    helperText={errors.email?.message}    
+                    label='Correo' variant='filled' fullWidth></TextField>
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField {...register('password')} label='Contraseña' type='password' variant='filled' fullWidth></TextField>
+                    <TextField {...register('password',{
+                    required: 'Este campo es requerido',
+                    minLength: {value: 6,message: 'Debe tener al menos 6 caracteres'}
+                    })}
+                    error={!!errors.password}
+                    helperText={errors.password?.message}  
+                    label='Contraseña' type='password' variant='filled' fullWidth></TextField>
                 </Grid>
                 <Grid item xs={12}>
                     <Button type='submit' color='secondary' className='circular-btn' fullWidth size='large'>
@@ -37,7 +50,7 @@ type Inputs = {
                 </Grid>
                 <Grid item xs={12} display='flex' justifyContent='end'>
                     <Link href='/auth/register' style={{textDecoration:'underline'}}>
-                        ¿No tines cuenta?
+                        ¿No tienes cuenta?
                     </Link>
                 </Grid>
             </Grid>
