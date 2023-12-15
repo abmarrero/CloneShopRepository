b8318b5933b16998f96eba21cb48e6da4a06a695
abmarrero <a.ernestomarrerol@gmail.com>
2023-12-15 16:57:03 -0500

    show loginerror

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -3,10 +3,11 @@
 import { cloneApi } from '@/api';
 import { AuthLayout } from '@/components/layouts'
 import { isEmail } from '@/utils/validations';
-import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Button, Chip, Grid, Link, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
+import { ErrorOutline } from '@mui/icons-material';
 
 type Inputs = {
     email: string,
@@ -15,20 +16,32 @@ type Inputs = {
   
   const LoginPage = () => {
       const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-      
+
+      const [showError, setShowError] = useState(false)
+      const [botonBloqueado, setBotonBloqueado] = useState(false);
         const onLoginUser = async({email, password}:Inputs) => {
             try {
-                
+                setShowError(false);
+                const manejarClic = () => {
+                    
+                }
                 const {data} = await cloneApi.post('/user/login', {email, password});
                 const {token, user} = data;
                 console.log({user, token});
             } catch (error) {
                 console.log('Error en las credenciales');
+                setBotonBloqueado(true);
+                setShowError(true);
+                setTimeout(() => setShowError(false), 2000);
                 // if(axios.isAxiosError(error)) {
                 //     error.message;
                 // }    
+                setTimeout(() => {
+                    setBotonBloqueado(false); // Desbloquear el botón después de 3 segundos
+                  }, 1000);
+                }
             }
-        };
+       
   return (
     <AuthLayout title={'Ingresar'} >
         <form onSubmit={handleSubmit(onLoginUser)} noValidate>
@@ -36,6 +49,7 @@ type Inputs = {
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <Typography variant='h1' component='h1'>Iniciar Sesión</Typography>
+                    
                 </Grid>
                 <Grid item xs={12}>
                     <TextField type="email" {...register('email',{
@@ -56,11 +70,23 @@ type Inputs = {
                     label='Contraseña' type='password' variant='filled' fullWidth></TextField>
                 </Grid>
                 <Grid item xs={12}>
-                    <Button type='submit' color='secondary' className='circular-btn' fullWidth size='large'>
+                    <Button type='submit' color='secondary' className='circular-btn' fullWidth size='large' 
+                    disabled = {botonBloqueado ? true : false}
+                    >
                         Ingresar
                     </Button>
                 </Grid>
+                <Grid item xs={12}>
+                <Chip
+                    label= 'No reconocemos ese usuario/contraseña'
+                    color='error'
+                    icon={<ErrorOutline/>}
+                    className='fadeIn'
+                    sx={{display:showError ? 'flex' : 'none'}}
+                    />
+                </Grid>
                 <Grid item xs={12} display='flex' justifyContent='end'>
+                
                     <Link href='/auth/register' style={{textDecoration:'underline'}}>
                         ¿No tienes cuenta?
                     </Link>
